5dc32450dc275a59f094df78c6ec401ff83b8eaa
mfarhanr <muhammadfarhanr09@gmail.com>
2024-10-24 14:36:11 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,45 @@
+import java.util.Scanner
+
 public class Nomor2{
     public static void main(String[] args){
+    String Labubu, Jenglot, Voodo, barang;
+        double harga = 0;
+        double diskon = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukan nama produk [Labubu/Jenglot/kita]: ");
+        Labubu = scanner.next();
+        System.out.print("Masukan nama produk [Labubu/Jenglot/kita]: ");
+        Jenglot= scanner.next();
+        System.out.print("Masukan nama produk [Labubu/Jenglot/kita]: ");
+        Labubu = scanner.next();
+
+        switch (barang) {
+            case "Labubu":
+              harga += 300000;
+              break;
+            case "Jenglot":
+              harga += 250000;
+              break;
+            case "Voodo":
+              harga += 200000;
+              break;
+            default:
+            System.out.println("Input lagi (Y/N)?: ");
+
+          }
+
+        if (harga >500000) {
+            diskon = harga*0.10;
+        } else if (harga >=100000) {
+            diskon = harga*0.15;
+        }
+
+        System.out.println("Jumlah pembelian: " + barang);
+
+        System.out.println("Diskon " + diskon);
+        System.out.println("Jumlah uang yang diterima: " + harga);
 
     }
 }
