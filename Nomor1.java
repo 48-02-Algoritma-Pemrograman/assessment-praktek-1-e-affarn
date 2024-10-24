a464e103dade9c682467fe133aa54260ff0dcd4f
mfarhanr <muhammadfarhanr09@gmail.com>
2024-10-24 14:47:51 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class Nomor1{
     public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
-    int i = 0;
-    double totalTunjangan = 0;
-    String JmlKeluargaLebihSamaDengan3
-
+        int i = 0;
+        double totalTunjangan = 0;
+        boolean jmlKeluargaLbhSmTiga;
         System.out.println("Jumlah Anggota Keluarga: ");
-        jmlKeluargaLebihSamaDengan3 = scanner.nextLine();
-        System.out,println("Besar Tunjangan Kesehatan);
+        jmlKeluargaLbhSmTiga = scanner.nextBoolean();
+        System.out.println("Besar Tunjangan Kesehatan: ");
+        totalTunjangan = scanner.nextDouble();
+
+        if (i >= 3) {
+            System.out.println("Besar Tunjangan Kesehatan: " + jmlKeluargaLbhSmTiga);
+        } else {
+        }
     }
 }
